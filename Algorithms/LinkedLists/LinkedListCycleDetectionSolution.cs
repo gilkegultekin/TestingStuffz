3329171212890532ea74ad1d2974a6fd9c6f6b1c
Ilke Gultekin <gilkegultekin@gmail.com>
2019-04-18 23:03:45 +0300

    - linked list cycle detection

---
+++ Algorithms/LinkedLists/LinkedListCycleDetectionSolution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.LinkedLists
+{
+    public class LinkedListCycleDetectionSolution : ISolution<bool, ListNode>
+    {
+        public bool HasCycle(ListNode head)
+        {
+            HashSet<ListNode> nodeHashSet = new HashSet<ListNode>();
+
+            ListNode current = head;
+            nodeHashSet.Add(current);
+
+            while(current.next != null){
+                current = current.next;
+                if (nodeHashSet.Contains(current)){
+                    return true;
+                }
+                else{
+                    nodeHashSet.Add(current);
+                }
+            }
+
+            return false;
+        }
+
+        public bool Solve(ListNode param)
+        {
+            return HasCycle(param);
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/LinkedListCycleDetectionSolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Algorithms.LinkedLists;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class LinkedListCycleDetectionSolutionTests
+    {
+        [Fact]
+        public void SingleNodeList_ShouldNotDetectCycle(){
+            ListNode head = new ListNode(3);
+
+            LinkedListCycleDetectionSolution solution = new LinkedListCycleDetectionSolution();
+            bool hasCycle = solution.Solve(head);
+
+            hasCycle.Should().BeFalse();
+        }
+    }
+}
