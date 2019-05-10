2aad2b1319bbe39007d80b1bae0d2224c0267b2f
Ilke Gultekin <gilkegultekin@gmail.com>
2019-05-10 09:17:42 +0300

    Added a solution for the Single Number problem.

---
+++ Algorithms/SingleNumberSolution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+    /// 
+    /// Note:
+    /// 
+    /// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+    /// 
+    /// Example 1:
+    /// 
+    /// Input: [2,2,1]
+    /// Output: 1
+    /// Example 2:
+    /// 
+    /// Input: [4,1,2,1,2]
+    /// Output: 4
+    /// </summary>
+    public class SingleNumberSolution : ISolution<int, int[]>
+    {
+        public int Solve(int[] param)
+        {
+            return SingleNumber(param);
+        }
+
+        public int SingleNumber(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            //XOR everything
+
+            int result = nums[0] ^ nums[1];
+            for (int i = 2; i < nums.Length; i++)
+            {
+                result = result ^ nums[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/SingleNumberSolutionTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class SingleNumberSolutionTests
+    {
+        [Fact]
+        public void Array1()
+        {
+            int[] array = {2,2,1};
+            const int expectedResult = 1;
+
+            SingleNumberSolution solution = new SingleNumberSolution();
+            int result = solution.Solve(array);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public void Array2()
+        {
+            int[] array = { 4, 1, 2, 1, 2 };
+            const int expectedResult = 4;
+
+            SingleNumberSolution solution = new SingleNumberSolution();
+            int result = solution.Solve(array);
+
+            result.Should().Be(expectedResult);
+        }
+    }
+}
