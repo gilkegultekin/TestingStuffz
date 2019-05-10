001bf0b92eb59bb4c75f00abefeca8a96e26854b
Ilke Gultekin <gilkegultekin@gmail.com>
2019-05-10 14:37:55 +0300

    Refactored some test methods.

--- tests/Algorithms.Tests/MissingNumberSolutionTests.cs
+++ tests/Algorithms.Tests/MissingNumberSolutionTests.cs
@@ -6,28 +6,28 @@ namespace Algorithms.Tests
     public class MissingNumberSolutionTests
     {
         [Theory]
-        [InlineData(new[] { 3, 0, 1 }, 2, MissingNumberStrategy.SumV2)]
-        [InlineData(new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }, 8, MissingNumberStrategy.SumV2)]
-        [InlineData(new[] { 0 }, 1, MissingNumberStrategy.SumV2)]
-        [InlineData(new[] { 0, 1 }, 2, MissingNumberStrategy.SumV2)]
-        [InlineData(new[] { 1 }, 0, MissingNumberStrategy.SumV2)]
-        public void Test_WithSumV2(int[] array, int expectedResult, MissingNumberStrategy strategy)
+        [InlineData(new[] { 3, 0, 1 }, 2)]
+        [InlineData(new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }, 8)]
+        [InlineData(new[] { 0 }, 1)]
+        [InlineData(new[] { 0, 1 }, 2)]
+        [InlineData(new[] { 1 }, 0)]
+        public void Test_WithSumV2(int[] array, int expectedResult)
         {
-            MissingNumberSolution solution = new MissingNumberSolution(strategy);
+            MissingNumberSolution solution = new MissingNumberSolution(MissingNumberStrategy.SumV2);
             int result = solution.Solve(array);
 
             result.Should().Be(expectedResult);
         }
 
         [Theory]
-        [InlineData(new[] { 3, 0, 1 }, 2, MissingNumberStrategy.Xor)]
-        [InlineData(new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }, 8, MissingNumberStrategy.Xor)]
-        [InlineData(new[] { 0 }, 1, MissingNumberStrategy.Xor)]
-        [InlineData(new[] { 0, 1 }, 2, MissingNumberStrategy.Xor)]
-        [InlineData(new[] { 1 }, 0, MissingNumberStrategy.Xor)]
-        public void Test_WithXor(int[] array, int expectedResult, MissingNumberStrategy strategy)
+        [InlineData(new[] { 3, 0, 1 }, 2)]
+        [InlineData(new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }, 8)]
+        [InlineData(new[] { 0 }, 1)]
+        [InlineData(new[] { 0, 1 }, 2)]
+        [InlineData(new[] { 1 }, 0)]
+        public void Test_WithXor(int[] array, int expectedResult)
         {
-            MissingNumberSolution solution = new MissingNumberSolution(strategy);
+            MissingNumberSolution solution = new MissingNumberSolution(MissingNumberStrategy.Xor);
             int result = solution.Solve(array);
 
             result.Should().Be(expectedResult);
