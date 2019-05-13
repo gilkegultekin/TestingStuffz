dd2bf76f8e0d74f77e3bcf146d755a454ea0a9d2
Ilke Gultekin <gilkegultekin@gmail.com>
2019-05-13 13:08:35 +0300

    Added Binary Tree Pruning solution.

--- tests/Algorithms.Tests/BinaryTreeMergeSolutionTests.cs
+++ tests/Algorithms.Tests/BinaryTreeMergeSolutionTests.cs
@@ -1,6 +1,7 @@
 ﻿using Algorithms.BST;
 using FluentAssertions;
 using Xunit;
+using BSTTreeNode = Algorithms.BST.TreeNode;
 
 namespace Algorithms.Tests
 {
@@ -12,11 +13,11 @@ namespace Algorithms.Tests
             const int firstRootValue = 5;
             const int secondRootValue = 10;
 
-            TreeNode firstRoot = new TreeNode(firstRootValue);
-            TreeNode secondRoot = new TreeNode(secondRootValue);
+            BSTTreeNode firstRoot = new BSTTreeNode(firstRootValue);
+            BSTTreeNode secondRoot = new BSTTreeNode(secondRootValue);
 
             BinaryTreeMergeSolution solution = new BinaryTreeMergeSolution();
-            TreeNode mergedRoot = solution.Solve(firstRoot, secondRoot);
+            BSTTreeNode mergedRoot = solution.Solve(firstRoot, secondRoot);
 
             mergedRoot.Should().NotBeNull();
             mergedRoot.left.Should().BeNull();
@@ -31,13 +32,13 @@ namespace Algorithms.Tests
             const int secondRootValue = 10;
             const int leftChildValue = 2;
 
-            TreeNode firstRoot = new TreeNode(firstRootValue);
-            TreeNode secondRoot = new TreeNode(secondRootValue);
-            TreeNode leftChild = new TreeNode(leftChildValue);
+            BSTTreeNode firstRoot = new BSTTreeNode(firstRootValue);
+            BSTTreeNode secondRoot = new BSTTreeNode(secondRootValue);
+            BSTTreeNode leftChild = new BSTTreeNode(leftChildValue);
             firstRoot.left = leftChild;
 
             BinaryTreeMergeSolution solution = new BinaryTreeMergeSolution();
-            TreeNode mergedRoot = solution.Solve(firstRoot, secondRoot);
+            BSTTreeNode mergedRoot = solution.Solve(firstRoot, secondRoot);
 
             mergedRoot.Should().NotBeNull();
             mergedRoot.left.Should().NotBeNull();
@@ -52,46 +53,46 @@ namespace Algorithms.Tests
         public void MergeCustomTrees()
         {
             //create left tree
-            TreeNode leftTreeRoot = new TreeNode(1);
-            TreeNode leftTreeNode1 = new TreeNode(3);
+            BSTTreeNode leftTreeRoot = new BSTTreeNode(1);
+            BSTTreeNode leftTreeNode1 = new BSTTreeNode(3);
             leftTreeRoot.left = leftTreeNode1;
-            TreeNode leftTreeNode2 = new TreeNode(2);
+            BSTTreeNode leftTreeNode2 = new BSTTreeNode(2);
             leftTreeRoot.right = leftTreeNode2;
-            TreeNode lefTreeNode3 = new TreeNode(5);
+            BSTTreeNode lefTreeNode3 = new BSTTreeNode(5);
             leftTreeNode1.left = lefTreeNode3;
 
             //create right child
-            TreeNode rightTreeRoot = new TreeNode(2);
-            TreeNode rightTreeNode1 = new TreeNode(1);
+            BSTTreeNode rightTreeRoot = new BSTTreeNode(2);
+            BSTTreeNode rightTreeNode1 = new BSTTreeNode(1);
             rightTreeRoot.left = rightTreeNode1;
-            TreeNode rightTreeNode2 = new TreeNode(3);
+            BSTTreeNode rightTreeNode2 = new BSTTreeNode(3);
             rightTreeRoot.right = rightTreeNode2;
-            TreeNode rightTreeNode3 = new TreeNode(4);
+            BSTTreeNode rightTreeNode3 = new BSTTreeNode(4);
             rightTreeNode1.right = rightTreeNode3;
-            TreeNode rightTreeNode4 = new TreeNode(7);
+            BSTTreeNode rightTreeNode4 = new BSTTreeNode(7);
             rightTreeNode2.right = rightTreeNode4;
 
             BinaryTreeMergeSolution solution = new BinaryTreeMergeSolution();
-            TreeNode mergedRoot = solution.Solve(leftTreeRoot, rightTreeRoot);
+            BSTTreeNode mergedRoot = solution.Solve(leftTreeRoot, rightTreeRoot);
 
             mergedRoot.Should().NotBeNull();
             mergedRoot.val.Should().Be(3);
 
-            TreeNode mergedRootLeft = mergedRoot.left;
+            BSTTreeNode mergedRootLeft = mergedRoot.left;
             mergedRootLeft.Should().NotBeNull();
             mergedRootLeft.val.Should().Be(4);
 
-            TreeNode mergedRootRight = mergedRoot.right;
+            BSTTreeNode mergedRootRight = mergedRoot.right;
             mergedRootRight.Should().NotBeNull();
             mergedRootRight.val.Should().Be(5);
 
-            TreeNode mergedRootLeftLeft = mergedRootLeft.left;
+            BSTTreeNode mergedRootLeftLeft = mergedRootLeft.left;
             mergedRootLeftLeft.Should().NotBeNull();
             mergedRootLeftLeft.val.Should().Be(5);
             mergedRootLeftLeft.left.Should().BeNull();
             mergedRootLeftLeft.right.Should().BeNull();
 
-            TreeNode mergedRootLeftRight = mergedRootLeft.right;
+            BSTTreeNode mergedRootLeftRight = mergedRootLeft.right;
             mergedRootLeftRight.Should().NotBeNull();
             mergedRootLeftRight.val.Should().Be(4);
             mergedRootLeftRight.left.Should().BeNull();
@@ -99,7 +100,7 @@ namespace Algorithms.Tests
 
             mergedRootRight.left.Should().BeNull();
 
-            TreeNode mergedRootRightRight = mergedRootRight.right;
+            BSTTreeNode mergedRootRightRight = mergedRootRight.right;
             mergedRootRightRight.Should().NotBeNull();
             mergedRootRightRight.val.Should().Be(7);
             mergedRootRightRight.left.Should().BeNull();
---
+++ tests/Algorithms.Tests/BinaryTreePruningSolutionTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class BinaryTreePruningSolutionTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            TreeNode root = new TreeNode(1);
+            TreeNode rootRight = new TreeNode(0);
+            root.right = rootRight;
+            TreeNode rootRightLeft = new TreeNode(0);
+            rootRight.left = rootRightLeft;
+            TreeNode rootRightRight = new TreeNode(1);
+            rootRight.right = rootRightRight;
+
+            BinaryTreePruningSolution solution = new BinaryTreePruningSolution();
+            TreeNode result = solution.Solve(root);
+
+            result.Should().NotBeNull();
+            result.left.Should().BeNull();
+            result.right.Should().NotBeNull();
+            result.right.left.Should().BeNull();
+            result.right.right.Should().NotBeNull();
+            result.right.right.left.Should().BeNull();
+            result.right.right.right.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            TreeNode root = new TreeNode(1);
+            TreeNode rootLeft = new TreeNode(0);
+            root.left = rootLeft;
+            TreeNode rootRight = new TreeNode(1);
+            root.right = rootRight;
+            TreeNode rootLeftLeft = new TreeNode(0);
+            rootLeft.left = rootLeftLeft;
+            TreeNode rootLeftRight = new TreeNode(0);
+            rootLeft.right = rootLeftRight;
+            TreeNode rootRightLeft = new TreeNode(0);
+            rootRight.left = rootRightLeft;
+            TreeNode rootRightRight = new TreeNode(1);
+            rootRight.right = rootRightRight;
+
+            BinaryTreePruningSolution solution = new BinaryTreePruningSolution();
+            TreeNode result = solution.Solve(root);
+
+            result.Should().NotBeNull();
+            result.left.Should().BeNull();
+            result.right.Should().NotBeNull();
+            result.right.left.Should().BeNull();
+            result.right.right.Should().NotBeNull();
+            result.right.right.left.Should().BeNull();
+            result.right.right.right.Should().BeNull();
+        }
+    }
+}
